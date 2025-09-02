86bf286501a0fb1e85e864fd51d4b0324a31c61a
nusaibnull <73007610+nusaibnull@users.noreply.github.com>
2025-09-02 22:28:11 +0600

    Update dirsearch_run.bat

--- dirsearch_run.bat
+++ dirsearch_run.bat
@@ -6,10 +6,11 @@ set /p max="Enter how many times to run (e.g. 20): "
 
 :: Loop from 1 to %max%
 for /L %%i in (1,1,%max%) do (
-    start "DirSearch: %%i" cmd /k "py -3 dirsearch/dirsearch.py -l reports/%%i.txt --async --recursive --random-agent -i 200,301,302 -e php,aspx,log,env, -o output/dirsearch/%%i.txt"
+    start "DirSearch: %%i" cmd /k "py -3 dirsearch/dirsearch.py -l reports/%%i.txt --async --deep-recursive --random-agent -i 200,301,302 -e php,aspx,log,env, -o output/dirsearch/%%i.txt"
 	
     timeout /t 5 >nul
 )
 
 echo [✓] All tasks started.
 pause
+
